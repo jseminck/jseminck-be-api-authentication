3ac7322f89b09bbb0d552c98ef6518a643c136c3
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-08-20 18:09:36 +0300

    Another test

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@ mkdir jseminck-be-api-authentication
 mv ./jseminck-be-api-authentication.tgz jseminck-be-api-authentication/jseminck-be-api-authentication.tgz
 cd jseminck-be-api-authentication
 tar -zxvf jseminck-be-api-authentication.tgz
+node-gyp rebuild
 npm install
 npm run stop
 npm run start
