8ae69df132d67d6fcc24d1225b8a65923ec75d18
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-08-20 09:33:22 +0300

    Fix filename

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 mkdir jseminck-be-api-authentication
-mv ./jseminck-be-api-authentication.tgz jseminck-be-main/jseminck-be-api-authentication.tgz
+mv ./jseminck-be-api-authentication.tgz jseminck-be-api-authentication/jseminck-be-api-authentication.tgz
 cd jseminck-be-api-authentication
 tar -zxvf jseminck-be-api-authentication.tgz
 docker build -t jseminck/api-authentication .
