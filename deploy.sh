c52377e22a4e6808e0c3889229705f114815bcf3
Joachim Seminck <joachim.seminck@zeroturnaround.com>
2016-08-20 09:29:07 +0300

    Try docker container

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ mkdir jseminck-be-api-authentication
 mv ./jseminck-be-api-authentication.tgz jseminck-be-main/jseminck-be-api-authentication.tgz
 cd jseminck-be-api-authentication
 tar -zxvf jseminck-be-api-authentication.tgz
-npm i
-npm run start
+docker build -t jseminck/api-authentication .
+docker run -p 9200:8080 -d jseminck/api-authentication
 rm jseminck-be-api-authentication.tgz
